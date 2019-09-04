9a0c1bd1b8c50f9a144e00adb14e3e6b38d8eb6e
rklimavicius <53342252+rklimavicius@users.noreply.github.com>
2019-09-04 20:10:59 +0300

    Add files via upload

--- src/test/java/framework/tests/BaseTest.java
+++ src/test/java/framework/tests/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,13 +15,25 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     WebDriver driver;
+    private static String browser = "Firefox";
 
     @BeforeEach
     public void setup(){
         //use Chrome Driver
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        if (this.browser=="Chrome")
+        {
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        }
+
+        if (this.browser=="Firefox")
+        {
+            System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+            driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        }
     }
 
     protected void WaitForElementToBeClickable(WebElement by)
